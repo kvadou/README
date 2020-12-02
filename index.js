ba357dadd73ef1c735e2d636ddf77582ed2944ca
DPK <dougkvamme@gmail.com>
2020-12-02 12:20:05 -0600

    added more dependencies

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // Dependencies & variables
 const fs = require("fs");
+const util = require("util");
 const inquirer = require("inquirer");
+const generateReadme = require("./utils/generateReadme");
+const writeFileAsync = util.promisify(fs.writeFile);
