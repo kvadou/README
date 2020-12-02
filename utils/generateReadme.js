defe798f36fa64c2a470e14092e7c6c904b57972
DPK <dougkvamme@gmail.com>
2020-12-02 13:38:16 -0600

    finishing the generate readme.js

--- utils/generateReadme.js
+++ utils/generateReadme.js
@@ -8,7 +8,39 @@ function generateReadme(answers) {
 ## Description
 🔍 ${answers.description}
 
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
 
+## Installation
+💾 ${answers.installation}
 
+## Usage
+💻 ${answers.usage}
+
+## License
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+<br />
+This application is covered by the ${answers.license} license. 
+
+## Contributing
+👪 ${answers.contributing}
+
+## Tests
+✏️ ${answers.tests}
+
+## Questions
+✋ ${answers.questions}<br />
+<br />
+:octocat: Find me on GitHub: [${answers.username}](https://github.com/${answers.username})<br />
+<br />
+✉️ Email me with any questions: ${answers.email}<br /><br />
+
+_This README was generated with ❤️ by [README-generator](https://github.com/kvadou/README) 🔥🔥🔥_
     `;
 }
