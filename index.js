300483fc1e8b894c78bd03c6310c9335b13398bc
DPK <dougkvamme@gmail.com>
2020-12-02 12:36:00 -0600

    more user question prompts

--- index.js
+++ index.js
@@ -13,6 +13,46 @@ function promptUser() {
       name: "projectTitle",
       message: "What is the project title?",
     },
-    {},
+    {
+      type: "input",
+      name: "description",
+      message: "Write a brief description of your project: ",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Describe the installation process if any: ",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "What is this projects usage for?",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "Choose the appropriate license for this project: ",
+      choices: ["Apache", "Academic", "GNU", "ISC", "MIT", "Mozilla", "Open"],
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "Who are the contributors of this project?",
+    },
+    {
+      type: "input",
+      name: "questions",
+      message: "What do I do if I have an issue? ",
+    },
+    {
+      type: "input",
+      name: "username",
+      message: "Please enter your GitHub username: ",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Please enter your email: ",
+    },
   ]);
 }
