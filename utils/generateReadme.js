ed3736d2daec1250d1f4cab9fa31a2149bb36ff0
DPK <dougkvamme@gmail.com>
2020-12-02 13:35:15 -0600

    adding string to generate readme.js

--- utils/generateReadme.js
+++ utils/generateReadme.js
@@ -1,2 +1,14 @@
 // generateReadme function that will populate the README.md
-function generateReadme(answers) {}
+function generateReadme(answers) {
+  return `
+<h1 align="center">${answers.projectTitle} 👋</h1>
+
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)<br />
+
+## Description
+🔍 ${answers.description}
+
+
+
+    `;
+}
